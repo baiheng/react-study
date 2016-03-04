fda2b31ed740cca4d0de18abdf4b60b281f40090
chenbh <chenbh@jpush.cn>
2016-03-04 21:32:46 +0800

    完成第一版

--- material_ui_study/src/main.js
+++ material_ui_study/src/main.js
@@ -1,24 +1,26 @@
 import React, { PropTypes, Component } from 'react'
 
 import Head from './head'
-import LeftMenu from './left_menu/left_menu'
+import LeftMenu from './left_menu'
+import Body from './main_content' 
 
 
 class App extends Component {
     render() {
         return (
             <div>
-                <div style={{ position: "fixed", width: "100%" }}>
+                <div style={{ position: "fixed", width: "100%", zIndex: 100 }}>
                     <Head />
                 </div>
-                <div style={{
-                    overflow: "auto",
-                    position: "fixed",
-                    top: "66px",
-                    width: "250px",
-                    bottom: 0,
-                    borderRight: "1px solid #A52A2A",}}>
-                    <LeftMenu />
+                <LeftMenu />
+                <div 
+                    style={{ 
+                        position: "relative",
+                        top: "66px",
+                        marginLeft: "260px", 
+                        padding: "20px",
+                }}>
+                    <Body />
                 </div>
             </div>
         )
