b9d76e5db37d82163e03d766b69e9f8e808b6099
chenbh <chenbh@jpush.cn>
2016-03-04 21:33:39 +0800

    结构化项目

---
+++ material_ui_study/src/left_menu/index.js
@@ -0,0 +1,89 @@
+import React, { PropTypes, Component } from 'react'
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import ActionGrade from 'material-ui/lib/svg-icons/action/grade';
+import ContentInbox from 'material-ui/lib/svg-icons/content/inbox';
+import ContentDrafts from 'material-ui/lib/svg-icons/content/drafts';
+import ContentSend from 'material-ui/lib/svg-icons/content/send';
+import Paper from 'material-ui/lib/paper';
+import { SelectableContainerEnhance } from 'material-ui/lib/hoc/selectable-enhance';
+let SelectableList = SelectableContainerEnhance(List);
+
+function wrapState(ComposedComponent) {
+    class StateWrapper extends Component{
+        constructor(props, context) {
+            super(props, context);
+            this.state = {selectedIndex: 1};
+        }
+
+        handleUpdateSelectedIndex(e, index) {
+            this.setState({
+                selectedIndex: index,
+            });
+        }
+
+        render() {
+            return (
+                <ComposedComponent
+                    {...this.props}
+                    {...this.state}
+                    valueLink={{
+                        value: this.state.selectedIndex,
+                        requestChange: this.handleUpdateSelectedIndex.bind(this)
+                    }}
+                />
+            );
+        }
+    }
+    return StateWrapper;
+}
+
+SelectableList = wrapState(SelectableList);
+
+class LeftMenu extends Component {
+    render() {
+        return (
+            <Paper zDepth={2} rounded={false} 
+            style={{
+                overflow: "auto",
+                position: "fixed",
+                top: "66px",
+                width: "250px",
+                bottom: 0,
+            }}>
+                <SelectableList subheader="Nested List Items">
+                    <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} value={1} />
+                    <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} value={2} />
+                    <ListItem primaryText="Inbox" leftIcon={<ContentInbox />}  value={3}
+                        primaryTogglesNestedList={true}
+                        nestedItems={[
+                            <ListItem
+                                key={1}
+                                value={4}
+                                primaryText="Starred"
+                                leftIcon={<ActionGrade />}
+                            />,
+                            <ListItem
+                                key={2}
+                                value={5}
+                                primaryText="Sent Mail"
+                                leftIcon={<ContentSend />}
+                                primaryTogglesNestedList={true}
+                                nestedItems={[
+                                    <ListItem 
+                                        key={1} 
+                                        primaryText="Drafts"
+                                        leftIcon={<ContentDrafts />}
+                                        value={6}
+                                    />,
+                                ]}
+                            />,
+                        ]}
+                    />
+                </SelectableList>
+            </Paper>
+        )
+    }
+}
+
+export default LeftMenu;
---
+++ material_ui_study/src/main_content/index.js
@@ -0,0 +1,37 @@
+import React, { PropTypes, Component } from 'react'
+
+
+class Body extends Component {
+    render() {
+        return (
+            <table className="am-table am-table-bordered am-table-hover">
+                <thead>
+                    <tr>
+                        <th>网站名称</th>
+                        <th>网址</th>
+                        <th>创建时间</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>John Smith</td>
+                        <td>Unemployed</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Stephanie Sanders</td>
+                        <td>Employed</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Steve Brown</td>
+                        <td>Employed</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default Body;
