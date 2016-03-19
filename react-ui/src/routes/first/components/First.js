6d7d43b0e77b26c16d150c81202fd82e1bfc33d9
465299904@qq.com <465299904@qq.com>
2016-03-20 01:34:50 +0800

    完成异步加载

---
+++ react-ui/src/routes/first/components/First.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+class First extends React.Component {
+  render() {
+    return (
+		<div>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            firstfirst<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            firstfirst<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+        </div>
+    )
+  }
+}
+
+module.exports = First
