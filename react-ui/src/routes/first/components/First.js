8abe37623092021294c50384d2aa5e677cde9cad
465299904@qq.com <465299904@qq.com>
2016-03-26 15:54:57 +0800

    合并master

---
+++ react-ui/src/routes/first/components/First.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Pagination, Table } from 'rctui'
+
+
+class First extends React.Component {
+  render() {
+    return (
+		<div>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            firstfirst<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            firstfirst<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+            first<br/>
+        </div>
+    )
+  }
+}
+
+module.exports = First
